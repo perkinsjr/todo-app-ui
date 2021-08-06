f8dd4c04d2d243d13d97b4ac8666a77b0ecc022d
James Perkins <jamesperkins@hey.com>
2021-08-06 14:38:21 -0400

    Adding all contents

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-
+import {ChakraProvider} from "@chakra-ui/react";
+import {PageShell} from "../components/Layout/Pageshell"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ return(
+   <ChakraProvider>
+     <PageShell>
+     <Component {...pageProps}/>
+     </PageShell>
+   </ChakraProvider>
+ )
 }
 
 export default MyApp
